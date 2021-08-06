18be0a1270d521acef63d678c1bf71a235efc727
Fido <fidencio.garrido@gmai.com>
2021-08-06 09:11:25 -0700

    Adding time diff

--- dist/index.js
+++ dist/index.js
@@ -6286,6 +6286,14 @@ var __webpack_exports__ = {};
 const core = __nccwpck_require__(24);
 const github = __nccwpck_require__(16);
 
+const MINUTE = 60000;
+const HOUR = MINUTE * 60;
+const DAY = HOUR * 24;
+
+function to(from, factor) {
+	return Math.floor(from / factor);
+}
+
 try {
 	const name = core.getInput('your-name');
 	console.log(`Hello ${name}`);
@@ -6294,10 +6302,18 @@ try {
 	const payload = JSON.stringify(github.context.payload, null, 2);
 	console.log(`Payload: ${payload}`);
 	const commits = Array.isArray(github.context.payload.commits) ? github.context.payload.commits : [];
+	let lastDate = null;
 	if (commits.length > 0) {
 		console.log(`ℹ️  Commits: ${commits.length}`);
 		commits.forEach(c => {
-			console.log(`- ${c.author.email} ${c.author.name} - ${c.message}`);
+			let sinceMsg = '';
+			const commitTS = new Date(c.timestamp);
+			if (lastDate !== null) {
+				const diff = to(commitTS.getTime() - lastDate.getTime(), MINUTE);
+				sinceMsg = `${diff.toLocaleString()} minutes after the prev commit`;
+			}
+			console.log(`- ${c.author.email} ${c.author.name} - ${c.message}. ${sinceMsg}`);
+			lastDate = commitTS;
 		});
 	}
 } catch(err) {
--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+const MINUTE = 60000;
+const HOUR = MINUTE * 60;
+const DAY = HOUR * 24;
+
+function to(from, factor) {
+	return Math.floor(from / factor);
+}
+
 try {
 	const name = core.getInput('your-name');
 	console.log(`Hello ${name}`);
@@ -9,10 +17,18 @@ try {
 	const payload = JSON.stringify(github.context.payload, null, 2);
 	console.log(`Payload: ${payload}`);
 	const commits = Array.isArray(github.context.payload.commits) ? github.context.payload.commits : [];
+	let lastDate = null;
 	if (commits.length > 0) {
 		console.log(`ℹ️  Commits: ${commits.length}`);
 		commits.forEach(c => {
-			console.log(`- ${c.author.email} ${c.author.name} - ${c.message}`);
+			let sinceMsg = '';
+			const commitTS = new Date(c.timestamp);
+			if (lastDate !== null) {
+				const diff = to(commitTS.getTime() - lastDate.getTime(), MINUTE);
+				sinceMsg = `${diff.toLocaleString()} minutes after the prev commit`;
+			}
+			console.log(`- ${c.author.email} ${c.author.name} - ${c.message}. ${sinceMsg}`);
+			lastDate = commitTS;
 		});
 	}
 } catch(err) {
