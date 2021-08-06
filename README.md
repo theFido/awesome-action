0ddf43c34236d73cf160bcdfd1ce799d22575b38
Fido <fidencio.garrido@gmai.com>
2021-08-05 19:52:54 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Demo JS action
+
+Say hi
+
+## The basics
+
+### Inputs
+- `your-name` (**Required**). Your name.
+
+### Outputs
+- `time`. Execution time
+
+## Example
+
+```
+uses: thefido/awesome-action@v1.0
+with:
+	your-name: 'Amazing Fido'
+```
---
+++ index.js
@@ -0,0 +1,13 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+	const name = core.getInput('your-name');
+	console.log(`Hello ${name}`);
+	const time = (new Date()).toLocaleTimeString();
+	core.setOutput('time', time);
+	const payload = jSON.stringify(github.context.payload, null, 2);
+	console.log(`Payload: ${payload}`);
+} catch(err) {
+	core.setFailed(error.message);
+}
