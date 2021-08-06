f9a02d9f34cd78ccefb4ca506f0b476c257becf3
Fido <fidencio.garrido@gmai.com>
2021-08-06 07:09:54 -0700

    Printing commits

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ try {
 	core.setOutput('time', time);
 	const payload = JSON.stringify(github.context.payload, null, 2);
 	console.log(`Payload: ${payload}`);
+	const commits = Array.isArray(github.context.payload.commits) ? github.context.payload.commits : [];
+	if (commits.length > 0) {
+		console.log(`ℹ️  Commits: ${commits.length}`);
+		commits.forEach(c => {
+			console.log(`- ${c.author.email} ${c.author.name} - ${c.message}`);
+		});
+	}
 } catch(err) {
 	core.setFailed(err.message);
 }
